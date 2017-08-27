d1c9390f45cd5221ccab287688d50b69c3f74728
Gallus AWA <Gallus awagdp@gmail.com>
2017-08-26 21:26:08 -0400

    second commit

--- app/src/main/java/com/example/gallusawa/datapersistance/Main2Activity.java
+++ app/src/main/java/com/example/gallusawa/datapersistance/Main2Activity.java
@@ -1,22 +1,53 @@
 package com.example.gallusawa.datapersistance;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private static final String TAG = "Main2Activity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
-
+        Log.d(TAG, "onCreate: ");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+            super.onResume();
+
+        Log.d(TAG, "onResume: ");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
 }
--- app/src/main/java/com/example/gallusawa/datapersistance/MainActivity.java
+++ app/src/main/java/com/example/gallusawa/datapersistance/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.gallusawa.datapersistance;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String MY_PREF_FILE = "mypref_file";
-    private static final String TAG = "Main";
-    EditText editText, editText2;
+    EditText etVal1, etVal2;
+    private static final String TAG = "MainActivity";
 
 
     @Override
@@ -21,26 +24,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.etVal1);
-        editText2 = (EditText) findViewById(R.id.etVal2);
+        etVal1 = (EditText) findViewById(R.id.etVal1);
+        etVal2 = (EditText) findViewById(R.id.etVal2);
 
 
     }
-    public void saveData(View view){
-        SharedPreferences sharedPrefernces = getSharedPreferences(MY_PREF_FILE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPrefernces.edit();
 
-         editor.putString("value", editText.getText().toString());
-        editor.putString("value2", editText2.getText().toString());
+    public void saveData(View view) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("value1", etVal1.getText().toString());
+        editor.putString("value2", etVal2.getText().toString());
         editor.commit();
 
-    }
-    public void getData(View view){
-        SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF_FILE, Context.MODE_PRIVATE);
-        Log.d(TAG, "getData" + sharedPreferences.getString("value", "defaut"));
-        Log.d(TAG, "getData: ");
+        Intent intent  = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 
+    public void getData(View view) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF_FILE, Context.MODE_PRIVATE);
+
+        sharedPreferences.getString("value1", "default in case this key returns nothing");
+        sharedPreferences.getString("value2", "default in case this key returns nothing");
+
+        Log.d(TAG, "getData: " + sharedPreferences.getString("value1", "default") + sharedPreferences.getString("value2", "default"));
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        Log.d(TAG, "onConfigurationChanged: ");
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            Toast.makeText(this, "Landscape", Toast.LENGTH_SHORT).show();
+
+        if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+            Toast.makeText(this, "Portrait", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToNextActivity(View view) {
+
+        switch (view.getId()){
+
+            case R.id.btnSaveData:
+                SharedPreferences sharedPreferences = getSharedPreferences(MY_PREF_FILE, Context.MODE_PRIVATE);
+                Intent intent = new Intent(this, Main2Activity.class);
+/*
+                intent.putExtra("savedData",sharedPreferences.getString("value1", "default in case this key returns nothing") +
+                        sharedPreferences.getString("value2", "default in case this key returns nothing"));
+*/
+                startActivity(intent);
+                break;
+        }
+
+    }
 
     @Override
     protected void onRestart() {
@@ -69,4 +111,3 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
-}
